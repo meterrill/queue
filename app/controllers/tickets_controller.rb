e12d451e0af2133be1165a5ecff8981a6d47a79d
Matthew Terrill <meterrill@gmail.com>
2015-06-01 21:56:36 +0000

    Add ability to edit and delete ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -20,6 +20,25 @@ class TicketsController < ApplicationController
     end
   end
 
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    if @ticket.update(ticket_params)
+      redirect_to tickets_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    redirect_to tickets_path
+  end
+
 private
   def ticket_params
     params.require(:ticket).permit(:description)
