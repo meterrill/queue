40b86c8b91f886b062c7ba38339fd0d74cb44204
Matthew Terrill <meterrill@gmail.com>
2015-06-01 19:21:31 +0000

    Add form for new tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,4 +6,22 @@ class TicketsController < ApplicationController
   def show
     @ticket = Ticket.find(params[:id])
   end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to tickets_path
+    else
+      render :new
+    end
+  end
+
+private
+  def ticket_params
+    params.require(:ticket).permit(:description)
+  end
 end
